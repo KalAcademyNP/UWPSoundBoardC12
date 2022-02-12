0c79ec5289c39ae84a6930d482810c933bd98ebc
Kal <info@kalacademy.org>
2022-02-12 12:22:56 -0800

    Sounds displayed in UI

--- UWPSoundBoard/Model/SoundManager.cs
+++ UWPSoundBoard/Model/SoundManager.cs
@@ -1,15 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace UWPSoundBoard.Model
 {
-    public class SoundManager
+    public static class SoundManager
     {
 
-        private List<Sound> getSounds()
+        public static void GetAllSounds(ObservableCollection<Sound> sounds)
+        {
+            var allSounds = getSounds();
+            sounds.Clear();
+
+            allSounds.ForEach(sound => sounds.Add(sound));
+        }
+
+        public static void GetSoundsByCategory(ObservableCollection<Sound> sounds, SoundCategory category)
+        {
+            var allSounds = getSounds();
+            var filteredSounds = allSounds.Where(sound => sound.Category == category).ToList();
+            sounds.Clear();
+
+            filteredSounds.ForEach(sound => sounds.Add(sound));
+
+        }
+
+        private static List<Sound> getSounds()
         {
             var sounds = new List<Sound>();
             sounds.Add(new Sound("Cow", SoundCategory.Animals));
